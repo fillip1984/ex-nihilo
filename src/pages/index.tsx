75e7d596462fb57cddead744924e3599b014040d
fillip1984 <fillip1984@gmail.com>
2023-06-05 14:42:28 -0400

    styling log in request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Head from "next/head";
+import { FaHourglassStart } from "react-icons/fa";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
@@ -12,20 +13,22 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-tr from-slate-600 to-slate-800 font-bold text-white">
-        <div className="mt-8">
-          {sessionData ? (
-            <h3>Hello {sessionData.user.name as string}</h3>
-          ) : (
+        {sessionData ? (
+          <h3 className="mt-8">Hello {sessionData.user.name as string}</h3>
+        ) : (
+          <div className="flex h-screen flex-col items-center justify-center gap-8">
             <h3>
               Please{" "}
               <button
                 className="rounded-full bg-white/10 px-4 py-2 font-semibold text-white no-underline transition hover:bg-white/20"
                 onClick={() => void signIn()}>
-                {sessionData ? "Sign out" : "Sign in"}
+                Sign in
               </button>
             </h3>
-          )}
-        </div>
+            <FaHourglassStart className="h-32 w-32" />
+            <h2>ex nihilo</h2>
+          </div>
+        )}
       </main>
     </>
   );
