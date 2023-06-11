096edd6b24562e390d44f3100fedd7e1ee020321
fillip1984 <fillip1984@gmail.com>
2023-06-11 13:12:01 -0400

    removed query logging locally and moved months to util

--- src/pages/routines/new/index.tsx
+++ src/pages/routines/new/index.tsx
@@ -15,6 +15,7 @@ import {
 import { routineFormSchema, type RoutineFormSchemaType } from "~/types";
 
 import { api } from "~/utils/api";
+import { months } from "~/utils/date";
 
 const NewRoutine = () => {
   const [occurrenceAnimation] = useAutoAnimate();
@@ -72,21 +73,6 @@ const NewRoutine = () => {
     };
     reset(newRoutine);
   }, [reset]);
-
-  const months = [
-    { name: "January", shortName: "Jan", number: 1 },
-    { name: "February", shortName: "Feb", number: 2 },
-    { name: "March", shortName: "Mar", number: 3 },
-    { name: "April", shortName: "Apr", number: 4 },
-    { name: "May", shortName: "May", number: 5 },
-    { name: "June", shortName: "June", number: 6 },
-    { name: "July", shortName: "July", number: 7 },
-    { name: "August", shortName: "Aug", number: 8 },
-    { name: "September", shortName: "Sept", number: 9 },
-    { name: "October", shortName: "Oct", number: 10 },
-    { name: "November", shortName: "Nov", number: 11 },
-    { name: "December", shortName: "Dec", number: 12 },
-  ];
 
   const occurrenceTypeWatch = useWatch({
     control,
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,21 @@
 export const yyyyMMddHyphenated = "yyyy-MM-dd";
 export const HH_mm_aka24hr = "HH:mm";
 
+export const months = [
+  { name: "January", shortName: "Jan", number: 1 },
+  { name: "February", shortName: "Feb", number: 2 },
+  { name: "March", shortName: "Mar", number: 3 },
+  { name: "April", shortName: "Apr", number: 4 },
+  { name: "May", shortName: "May", number: 5 },
+  { name: "June", shortName: "June", number: 6 },
+  { name: "July", shortName: "July", number: 7 },
+  { name: "August", shortName: "Aug", number: 8 },
+  { name: "September", shortName: "Sept", number: 9 },
+  { name: "October", shortName: "Oct", number: 10 },
+  { name: "November", shortName: "Nov", number: 11 },
+  { name: "December", shortName: "Dec", number: 12 },
+];
+
 export const nextNewYears = (date: Date) => {
   const nextNewYears = new Date(date.getFullYear() + 1, 0, 1);
   return nextNewYears;
