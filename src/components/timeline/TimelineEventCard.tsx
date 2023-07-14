13cfd1253f13231fb8c69ea6ef104dff94dbdbdf
fillip1984 <fillip1984@gmail.com>
2023-07-14 14:43:07 -0400

    condensing timeline view

--- src/components/timeline/TimelineEventCard.tsx
+++ src/components/timeline/TimelineEventCard.tsx
@@ -1,16 +1,26 @@
 import { format, formatDuration, isEqual } from "date-fns";
 import { BiCategory } from "react-icons/bi";
 import { BsBodyText } from "react-icons/bs";
-import { FaRegClock, FaStopwatch } from "react-icons/fa";
+import {
+  FaCaretDown,
+  FaChevronDown,
+  FaRegClock,
+  FaStopwatch,
+} from "react-icons/fa";
 import { MdCheck, MdNotInterested } from "react-icons/md";
 
 import { type TimelineEvent } from "~/types";
 import { api } from "~/utils/api";
 import { HH_mm_aka24hr } from "~/utils/date";
 import { IconAvatar } from "../topics/IconsAndColorHelpers";
+import { useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const TimelineEventCard = ({ event }: { event: TimelineEvent }) => {
   const utils = api.useContext();
+
+  const [cardAnimations] = useAutoAnimate();
+  const [showDetails, setShowDetails] = useState(false);
 
   const { mutate: completeAct, isLoading: isCompleting } =
     api.activities.complete.useMutation({
@@ -39,47 +49,68 @@ const TimelineEventCard = ({ event }: { event: TimelineEvent }) => {
     <div className="flex flex-col">
       <div className="flex gap-3 rounded-t-lg bg-white/10 p-2 transition duration-300 ease-in-out hover:bg-white/20">
         <IconAvatar iconValue={event.icon} color={event.color} />
-        <div className="flex flex-col">
-          <b>{event.name}</b>
-          <p className="flex items-center gap-2">
+        <div ref={cardAnimations} className="flex w-full flex-col">
+          <div className="flex items-center justify-between">
+            <b>{event.name}</b>
+            <button
+              onClick={() => setShowDetails((prev) => !prev)}
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-400">
+              <FaChevronDown
+                className={`text-xl transition-transform duration-300 ${
+                  showDetails ? "rotate-180" : ""
+                }`}
+              />
+            </button>
+          </div>
+          <p className="flex items-center gap-2 text-sm">
             <FaRegClock />
             {format(event.start, HH_mm_aka24hr)}
             {event.end && !isEqual(event.start, event.end)
               ? " - " + format(event.end, HH_mm_aka24hr)
               : ""}
           </p>
-          <div className="mt-2 flex flex-col gap-1 text-sm text-gray-400">
-            <p className="flex items-center gap-2">
-              <BsBodyText />
-              {event.description}
-            </p>
-            <p className="flex items-center gap-2">
-              <BiCategory />
-              {event.topicName}
-            </p>
+          {showDetails && (
+            <div>
+              <div className="mt-2 flex flex-col gap-1 text-sm text-gray-400">
+                <p className="flex items-center gap-2">
+                  <BsBodyText />
+                  {event.description}
+                </p>
+                <p className="flex items-center gap-2">
+                  <BiCategory />
+                  {event.topicName}
+                </p>
 
-            <p className="flex items-center gap-2">
-              <FaStopwatch />
-              {formatDuration(event.duration, {
-                format: [
-                  "years",
-                  "months",
-                  "weeks",
-                  "days",
-                  "hours",
-                  "minutes",
-                ],
-              })}
-              <span className="text-xs">
-                {event.type === "Suninfo" ? "(Length of day)" : "(Duration)"}
-              </span>
-            </p>
-          </div>
+                {event.type !== "Suninfo" && (
+                  <p className="flex items-center gap-2">
+                    <FaStopwatch />
+                    {formatDuration(event.duration, {
+                      format: [
+                        "years",
+                        "months",
+                        "weeks",
+                        "days",
+                        "hours",
+                        "minutes",
+                      ],
+                    })}
+                    <span className="text-xs">(Duration)</span>
+                  </p>
+                )}
+              </div>
+            </div>
+          )}
         </div>
       </div>
 
       {event.type === "Suninfo" && (
-        <div className="flex justify-end rounded-b-lg bg-slate-700 p-2 text-3xl">
+        <div className="flex items-center justify-between rounded-b-lg bg-slate-700 p-2">
+          <p>
+            Day time:{" "}
+            {formatDuration(event.duration, {
+              format: ["years", "months", "weeks", "days", "hours", "minutes"],
+            })}
+          </p>
           <a
             href="https://sunrise-sunset.org/"
             target="_blank"
